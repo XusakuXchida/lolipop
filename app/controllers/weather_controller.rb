67d47ce4452a55dc78ce70c1761980e73d5bc6e8
YUSAKU UCHIDA <ni34ga@yahoo.co.jp>
2019-01-15 00:01:56 +0900

    fix konnichiwa -> oyamasan

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,9 +1,9 @@
 class WeatherController < ApplicationController
 
   def create
-    require 'alexa_rubykit'
-    response = AlexaRUbykit::Response.new
-    response.add_speech("こんにちは")
-    response.build_response
+    request = AlexaRubykit::build_request(params)
+    response = AlexaRubykit::Response.new
+    response.add_speech("あれ？小山さん、まだそこの実装やってるんですか？")
+    render json: response.build_response
   end
 end
