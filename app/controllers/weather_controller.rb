719bc172b5122576afeccd361c0384f65e121727
YUSAKU UCHIDA <ni34ga@yahoo.co.jp>
2019-01-16 13:38:52 +0900

    add request.slot

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -3,7 +3,7 @@ class WeatherController < ApplicationController
   def create
     request = AlexaRubykit::build_request(params)
     response = AlexaRubykit::Response.new
-    response.add_speech("今日の天気は晴れ時々ぶたです")
+    response.add_speech("今日の天気は晴れ時々#{request.slots[:item][:value]}です")
     render json: response.build_response
 
     param = Param.new
